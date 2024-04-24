5e247361d8f87886a25e21e5e4633d248205a1d3
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2024-04-24 20:13:02 +0200

    configure nvim

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -113,8 +113,21 @@
     vimAlias=true;
     vimdiffAlias=true;
     extraConfig = ''
-    '';
+      set number
+      set cursorline
+      set title
+      set expandtab
+	    set tabstop=2
+      set softtabstop=2
+      set shiftwidth=2
+      " split navitation
+      nnoremap <C-J> <C-W><C-J>
+      nnoremap <C-K> <C-W><C-K>
+      nnoremap <C-L> <C-W><C-L>
+      nnoremap <C-H> <C-W><C-H>
+      '';
     plugins = with pkgs.vimPlugins; [
+      vim-airline
       {
         plugin = gruvbox-material;
         config = ''
