f0c83228274b354f33ef835a59d3d702c2eac347
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2024-06-27 08:35:25 +0200

    nix: pass inputs to submodules

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -11,6 +11,11 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+
+        # Set all inputs parameters as special arguments for all submodules,
+        # so you can directly use all dependencies in inputs in submodules
+        # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system#pass-non-default-parameters-to-submodules
+        { _module.args = { inherit inputs; }; }
       ];
     };
   };
