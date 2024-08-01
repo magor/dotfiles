f62eadd8e5f12c349c59c963ca8944d8ae90363f
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2024-08-01 15:02:42 +0200

    nix: stats module

---
+++ .config/nixos/modules/stats.nix
@@ -0,0 +1,22 @@
+{config, pkgs, ... }:
+
+{
+  services = {
+    netdata = {
+      enable = true;
+      config = {
+        global = {
+          # uncomment to reduce memory to 32 MB
+          #"page cache size" = 32;
+
+          # update interval
+          "update every" = 15;
+        };
+        ml = {
+          # enable machine learning
+          "enabled" = "yes";
+        };
+      };
+    };
+  };
+}
