035e189891e2a1f60b14b380117b99456c601d7a
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2024-11-12 12:41:06 +0100

    nix/virtmaster: remove unused

--- .config/nixos/hosts/virtmaster/default.nix
+++ .config/nixos/hosts/virtmaster/default.nix
@@ -55,41 +55,11 @@
     };
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.autoSuspend = false;
-  services.xserver.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   security.sudo.wheelNeedsPassword = false;
 
@@ -100,13 +70,9 @@
       description = "chyron";
       extraGroups = [ "networkmanager" ];
       packages = with pkgs; [
-      #  thunderbird
       ];
     };
   };
-
-  # Install firefox.
-  programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
