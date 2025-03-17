3ebab25c694c30a908e9cf026097cfcdc284597f
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2025-03-17 18:28:46 +0100

    nix: add missing syncthing module

---
+++ .config/nixos/modules/syncthing.nix
@@ -0,0 +1,21 @@
+{config, pkgs, ... }:
+
+{
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = "mirek";
+    dataDir = "/home/mirek/sync";
+
+    # Optional: set GUI credentials
+
+    # declarative settings fails for now
+    # see https://github.com/NixOS/nixpkgs/issues/245121
+    #settings.devices = {
+    #    "nixodeos" = { id = "..."; };
+    #};
+  };
+
+  # https://wiki.nixos.org/wiki/Syncthing#Disable_default_sync_folder
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder
+}
