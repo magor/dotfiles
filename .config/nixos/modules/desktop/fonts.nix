415262edd7570f0f9c9e070f1b970de24bb17165
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2025-03-17 17:03:10 +0100

    nixos/fonts: fix some websites

--- .config/nixos/modules/desktop/fonts.nix
+++ .config/nixos/modules/desktop/fonts.nix
@@ -10,6 +10,9 @@
       #corefonts # ms free fonts
       #dejavu_fonts
       #google-fonts
+      roboto roboto-mono # bitbucket web ui
+      helvetica-neue-lt-std # used on web
+      liberation_ttf
     ];
     fontconfig = {
       antialias = true;
@@ -20,6 +23,13 @@
       subpixel = {
         rgba = "rgb";
       };
+      localConf = ''
+        <!-- use a less horrible font substition for pdfs such as https://www.bkent.net/Doc/mdarchiv.pdf -->
+        <match target="pattern">
+          <test qual="any" name="family"><string>Helvetica</string></test>
+          <edit name="family" mode="assign" binding="same"><string>HelveticaNeueLTSTD</string></edit>
+        </match>
+      '';
     };
   };
 }
