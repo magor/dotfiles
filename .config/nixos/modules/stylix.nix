c0ae48598fe47013fa8d910af0ad37effcf6f4bc
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2025-05-15 13:28:56 +0200

    nix/stylix: smaller app fonts, medium contrast theme

--- .config/nixos/modules/stylix.nix
+++ .config/nixos/modules/stylix.nix
@@ -7,12 +7,13 @@
     image = ./wp.jpg;
     opacity.terminal = 0.95;
     fonts.sizes.terminal = 11;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-soft.yaml";
+    fonts.sizes.applications = 11;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
   };
   specialisation.light.configuration = {
     stylix = {
       polarity = lib.mkForce "light";
-      base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/gruvbox-material-light-hard.yaml";
+      base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/gruvbox-material-light-medium.yaml";
     };
   };
 }
