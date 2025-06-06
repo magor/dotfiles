4d848ebea4841c1c7d95172cd08caa5040fe088c
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2025-06-06 16:27:41 +0200

    nix/modules/common: cleanup

--- .config/nixos/modules/common/pkgs.nix
+++ .config/nixos/modules/common/pkgs.nix
@@ -11,17 +11,8 @@
     nvd
   ];
 
-  programs = {
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # mtr.enable = true;
-    # gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-  };
-
   # Generate the manual page index caches.
   # This allows searching for a page or keyword using utilities like apropos(1) and the -k option of man(1).
+  # disable if slow
   documentation.man.generateCaches = true;
 }
