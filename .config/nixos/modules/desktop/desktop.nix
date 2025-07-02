8d0548ff0aed9b03506ac5eb379395ddfebaa29d
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2025-07-02 18:20:55 +0200

    use rose-pine-hyprcursor

--- .config/nixos/modules/desktop/desktop.nix
+++ .config/nixos/modules/desktop/desktop.nix
@@ -12,6 +12,11 @@
     hypridle
     hyprpaper
     hyprcursor
+    rose-pine-hyprcursor
+    # for other cursor themes, check these:
+    # https://sakshatshinde.github.io/hyprcursor-themes/
+    # https://github.com/phisch/phinger-cursors
+    # https://github.com/search?q=hyprcursor&type=repositories
     hyprlock
     xdg-desktop-portal-hyprland
     sway
