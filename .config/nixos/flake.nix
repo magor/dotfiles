229ba2f7047c0f25df50ff657f11d35ebc7fdc9e
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2025-09-24 14:33:31 +0200

    nix/dell: use ide module

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -60,6 +60,7 @@
           ./modules/gaming.nix
           ./modules/wine.nix
           ./modules/chyron.nix
+          ./modules/ide.nix
           ./modules/looking-glass.nix
           ./modules/syncthing.nix
           ./modules/stylix.nix
