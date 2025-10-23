cc1429c3d75d737bab08b913f21bb209b776f7e2
Gajdos, Miroslav <miroslav.gajdos@chyron.com>
2025-10-23 12:51:27 +0200

    nix/server/stats: tailscalesd service discovery and scraper

--- .config/nixos/modules/server/stats.nix
+++ .config/nixos/modules/server/stats.nix
@@ -1,10 +1,49 @@
 { config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    tailscalesd # prometheus service discovery for tailscale
+  ];
+
   # https://nixos.org/manual/nixos/stable/#module-services-prometheus-exporters
   services.prometheus = {
     enable = true;
     listenAddress = "127.0.0.1";
+    globalConfig.scrape_interval = "10s"; # "1m"
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [
+          {
+            targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+          }
+        ];
+      }
+      {
+        job_name = "tailscale-node-exporter";
+        http_sd_configs = [
+          {
+            url = "http://localhost:9242/";
+          }
+        ];
+        relabel_configs = [
+          {
+            source_labels = [ "__meta_tailscale_device_hostname" ];
+            target_label = "tailscale_hostname";
+          }
+          {
+            source_labels = [ "__meta_tailscale_device_name" ];
+            target_label = "tailscale_name";
+          }
+          {
+            source_labels = [ "__address__" ];
+            # regex = "(.*)"; # default
+            replacement = "$1:9000";
+            target_label = "__address__";
+          }
+        ];
+      }
+    ];
   };
 
   # https://nixos.wiki/wiki/Grafana
@@ -23,6 +62,16 @@
         serve_from_sub_path = true;
       };
     };
+  };
+
+  systemd.services."tailscalesd" = {
+    description = "Prometheus Service Discovery for Tailscale";
+    after = [
+      "tailscale.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.ExecStart = "${pkgs.tailscalesd}/bin/tailscalesd -localapi -address 127.0.0.1:9242";
+    restartIfChanged = true;
   };
 
   systemd.services."tailscale-serve-grafana" = {
